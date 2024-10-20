005688827c322c50e1656daef7228b48f6b05571
N-i-ke <nike.forgithub@gmail.com>
2024-10-20 13:14:35 +0900

    fix: サイト全体の共通スタイルをtsxへ記載

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components"; // styledとcreateGlobalStyleを正しくインポート
 import "./App.css";
+import "./Animate.css";
 import Header from "./components/Header";
 import Video from "./components/Video";
 import HomePage from "./components/HomePage";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 
+// グローバルスタイルを作成
+const GlobalStyle = createGlobalStyle`
+  @charset "UTF-8";
+  html {
+    font-size: 100%;
+  }
+  .App{
+    overflow-x: hidden;
+  }
+  body {
+    font-size: 0.9rem;
+    line-height: 1.7;
+    margin: 0;
+    color: #f6f6e9;
+    background-color: rgba(28, 28, 28, 1);
+  }
+  a {
+    text-decoration: none;
+  }
+  img {
+    max-width: 100%;
+  }
+  li {
+    list-style: none;
+  }
+`;
+
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <div className="App">
         <Header />
         <Video />

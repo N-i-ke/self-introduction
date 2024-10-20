a6c9f19147e72bed0637de4ad591e9da4d097a36
N-i-ke <nike.forgithub@gmail.com>
2024-10-20 13:27:44 +0900

    fix: 各セクションのタイトルのスタイルをtsxへ記述

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,16 +1,38 @@
 import React from "react";
+import styled from "styled-components";
 
 interface TitleProps {
-    mainTitle: string;
-    subTitle: string;
+  mainTitle: string;
+  subTitle: string;
 }
 
-const SectionTitle: React.FC<TitleProps> = ({mainTitle,subTitle}) => {
+const SecTitle = styled.h2`
+  text-align: center;
+  font-size: 7rem;
+  color: #83ffe630;
+  position: relative;
+  letter-spacing: 5px;
+  font-weight: 900;
+`;
+
+const SubSecTitle = styled.span`
+  display: block;
+  font-size: 2.5rem;
+  color: #ffffff;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const SectionTitle: React.FC<TitleProps> = ({ mainTitle, subTitle }) => {
   return (
-    <h2 className="sec-title animated-div">
+    <SecTitle className="sec-title animated-div">
       {mainTitle}
-      <span className="sub-sec-title animated-li">{subTitle}</span>
-    </h2>
+      <SubSecTitle className="sub-sec-title animated-li">
+        {subTitle}
+      </SubSecTitle>
+    </SecTitle>
   );
 };
 

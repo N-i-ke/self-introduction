b9d819cac4197db2983b69a67079b1288fb2fd3f
N-i-ke <nike.forgithub@gmail.com>
2024-10-20 14:26:26 +0900

    fix: Footerスタイルをtsxへ記載

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+
+const FooterContainer = styled.footer`
+  text-align: center;
+  background-color: rgba(28, 28, 28, 1);
+  color: #fff;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .footer {
+    display: inline-block;
+    vertical-align: middle;
+    font-family: "Quicksand", sans-serif;
+  }
+
+  .btn-sns {
+    font-size: 5rem;
+    letter-spacing: 0;
+    color: #fff;
+    margin: 0;
+  }
+
+  p {
+    text-align: center;
+  }
+
+  i {
+    transition: all 0.5s;
+
+    &:hover {
+      color: #1da1f2;
+      transform: rotate(25deg);
+      transition: all 0.5s;
+    }
+  }
+`;
 
 const Footer: React.FC = () => {
   return (
-    <footer id="footer">
+    <FooterContainer id="footer">
       <p className="footer">&copy;2024 N-i-ke All Right Reserved.</p>
-    </footer>
+    </FooterContainer>
   );
 };
 

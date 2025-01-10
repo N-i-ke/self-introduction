d562c096bc8bcfb5383cc10b278ec5c240a9b33f
N-i-ke <82163707+N-i-ke@users.noreply.github.com>
2025-01-10 09:04:11 +0900

    fix: linkUrl

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -7,12 +7,12 @@ const Service: React.FC = () => {
         <i className="fas fa-edit animated"></i>
         <h3 className="animated">Webアプリケーション開発</h3>
         <p className="animated">
-          現職がWebアプリケーション開発のフロントエンドエンジニアという経歴から、React•TypeScriptを使ったWebアプリケーション開発を得意としています。アプリケーション開発のフロント側の開発はお任せください。
+          現職がWebアプリケーション開発のフロントエンドエンジニアという経歴から、React•TypeScriptを使ったWebアプリケーション開発を得意としています。
         </p>
       </div>
       <div className="service-wrap-right wrapper animated">
         <i className="fas fa-laptop-code animated"></i>
-        <h3 className="animated">LP•WEB•ECサイト制作 (WordPress/Shopify)</h3>
+        <h3 className="animated">LP•ECサイト制作 (WordPress/Shopify)</h3>
         <p className="animated">
           スマートフォンサイト、ワードプレスを使用した更新性の高いサイト、Shopifyを使用したECサイトなど、静的なサイトから動的なサイトまで様々なサイトのコーディングを行っております。
         </p>

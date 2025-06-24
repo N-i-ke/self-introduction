c6ae496e4d885a50c7a06ad4cfd57a1d5c7fef5c
K <82163707+N-i-ke@users.noreply.github.com>
2025-06-24 12:19:21 +0900

    refactor: 未使用のファイルを削除

--- src/components/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module './AuroraLoader' {
-  interface AuroraLoaderProps {
-    isLoading: boolean;
-    onLoadingComplete?: () => void;
-  }
-  
-  const AuroraLoader: React.FC<AuroraLoaderProps>;
-  export default AuroraLoader;
-} 
